6fb9ac33e2de21e015a8e45fac747d939e7cd7d9
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2024-12-10 15:40:18 -0300

    Added amantino configuration

---
+++ amantino-configuration.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      # Include desktop non-device-specific configuration.
+      ./custom.nix
+      # Include home manager configuration.
+      ./home-manager.nix
+    ];
+
+  boot = {
+    kernelModules = [ "ecryptfs" ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking.hostName = "amantino"; # Define your hostname.
+
+  environment.systemPackages = with pkgs; [
+    ecryptfs
+  ];
+  security.pam.enableEcryptfs = true;
+
+  users.motd = ''
+    Welcome to amantino.
+    "amantino" means "Lovely girl".
+  '';
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
